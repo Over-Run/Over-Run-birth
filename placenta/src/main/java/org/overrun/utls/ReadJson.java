e7d02b593304eaa9d77d4f71b0ce7a42429a2575
baka4n <qq474899581@gmail.com>
2022-05-16 00:31:42 +0800

    updata version json

---
+++ placenta/src/main/java/org/overrun/utls/ReadJson.java
@@ -0,0 +1,42 @@
+package org.overrun.utls;
+
+import java.io.*;
+
+public class ReadJson {
+	public static void readJsons(String name) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(name));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(name).getPath() + ".birth"));
+		var line = br.lines();
+		StringBuilder sb = new StringBuilder();
+		line.forEach(sb::append);
+		bw.write(sb.toString());
+		bw.close();
+		br.close();
+		/*while ((line = br.readLine()) != null) {
+			System.out.println("\n" +line);
+		}*/
+	}
+
+	public static void readS(String name) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(name));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(name + ".birth"));
+		var line = br.lines();
+		StringBuilder sb = new StringBuilder();
+		line.forEach(sb::append);
+		bw.write(sb.toString().replace("{", "").replace("}", "\n").replace("[", "\n").replace("\"id\":", "\"id\":").replace("\"type\":", "\n\"type\":").replace("\"url\":", "\n\"url\":").replace("\"time\":", "\n\"time\":"));
+		bw.close();
+		br.close();
+
+	}
+	public static void readSs(String name) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(name));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(name + ".birth"));
+		var line = br.lines();
+		StringBuilder sb = new StringBuilder();
+		line.forEach(sb::append);
+		bw.write(sb.toString().replace("{", "").replace("}", "\n").replace("[", "\n").replace(",", "\n"));
+		bw.close();
+		br.close();
+
+	}
+}
--- placenta/src/main/java/org/overrun/utls/Url.java
+++ placenta/src/main/java/org/overrun/utls/Url.java
@@ -2,6 +2,11 @@ package org.overrun.utls;
 
 import java.io.*;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.overrun.utls.ReadJson.readS;
+import static org.overrun.utls.ReadJson.readSs;
 
 public class Url {
 	public static final String url = "https://launchermeta.mojang.com/mc/game/version_manifest.json";
@@ -9,7 +14,7 @@ public class Url {
 	public static final char separatorChar = File.separatorChar;
 	public static void download(String url,String name) throws IOException {
 		var connection = new URL(url).openConnection();
-		BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(connection.getInputStream()));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 		BufferedWriter bw = new BufferedWriter(new FileWriter(name));
 		String line;
 		StringBuilder sb = new StringBuilder();
@@ -18,6 +23,8 @@ public class Url {
 			sb.append(line);
 		}
 		reader.close();
+		bw.close();
+
 	}
 
 
@@ -28,6 +35,109 @@ public class Url {
 		if (!new File(fileName).exists()) {
 			new File(fileName).mkdirs();
 		}
-		download(url, name);
+		if (!new File(name).exists()) {
+			download(url, name);
+		}
+		readS(name);
+		Map<String, String> version_manifest = getVersion_manifest_mcVersion(name);
+		Thread thread, thread1;
+		Runnable runnable, runnable1;
+		runnable = () -> {
+			for (var i : version_manifest.keySet()) {
+				if (i.contains("1.")) {
+					System.out.println(i + " " + version_manifest.get(i));
+					var versionFileName = user_dir + separatorChar + "minecraft"+ separatorChar + "versions" + separatorChar + i.replace(" ", "");
+					if (!new File(versionFileName).exists()) {
+						new File(versionFileName).mkdirs();
+					}
+					try {
+						var dw = versionFileName + separatorChar + i.replace(" ", "_") + ".json";
+						if (new File(dw).exists()) {
+							continue;
+						}
+						download(version_manifest.get(i), dw);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			for (var i : version_manifest.keySet()) {
+				var versionFileName = user_dir + separatorChar + "minecraft"+ separatorChar + "versions" + separatorChar + i.replace(" ", "");
+				if (!new File(versionFileName).exists()) {
+					new File(versionFileName).mkdirs();
+				}
+				if (i.contains("1.")) {
+					var dw = versionFileName + separatorChar + i.replace(" ", "_") + ".json";
+					try {
+						readSs(dw);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		thread = new Thread(runnable, "1.x.x");
+		thread.start();
+		runnable1 = () -> {
+			for (var i : version_manifest.keySet()) {
+				if (i.contains("w")) {
+					System.out.println(i + " " + version_manifest.get(i));
+					var versionFileName = user_dir + separatorChar + "minecraft"+ separatorChar + "versions" + separatorChar + i.replace(" ", "");
+					if (!new File(versionFileName).exists()) {
+						new File(versionFileName).mkdirs();
+					}
+					try {
+						var dw = versionFileName + separatorChar + i.replace(" ", "_") + ".json";
+						if (new File(dw).exists()) {
+							continue;
+						}
+						download(version_manifest.get(i), dw);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			for (var i : version_manifest.keySet()) {
+				var versionFileName = user_dir + separatorChar + "minecraft"+ separatorChar + "versions" + separatorChar + i.replace(" ", "");
+				if (!new File(versionFileName).exists()) {
+					new File(versionFileName).mkdirs();
+				}
+				if (i.contains("w")) {
+					var dw = versionFileName + separatorChar + i.replace(" ", "_") + ".json";
+					try {
+						readSs(dw);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		thread1 = new Thread(runnable1, "snapshot");
+		thread1.start();
+
+		/*ReadJson.formatter(name);*/
+	}
+
+	public static Map<String, String> getVersion_manifest_mcVersion(String name) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(name + ".birth"));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(name + ".copy.birth"));
+		String line;
+		Map<String, String> version_manifest = new HashMap<>();
+		String version = null, url = null;
+		while ((line = reader.readLine()) != null) {
+			if (line.contains("id")) {
+				version = line.split(":")[1].replace("\"", "").replace(",", "");
+			}
+			if (line.contains("url")) {
+				url = line.split(":")[1].replace("\"", "").replace(",", "") + ":" + line.split(":")[2].replace("\"", "").replace(",", "");
+			}
+			if (version != null && url != null) {
+				version_manifest.put(version, url);
+				version = null;
+				url = null;
+			}
+		}
+		reader.close();
+		return version_manifest;
 	}
 }
